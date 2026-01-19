fd31744d8fbd1523c6db70b30917697c2a37b328
Nigel Nindo <nigelnindodev@gmail.com>
2026-01-19 18:54:13 +0300

    Add pre-commit hooks for linting

--- server/src/main.ts
+++ server/src/main.ts
@@ -1,8 +1,14 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ConsoleLogger } from '@nestjs/common';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3030);
+  const app = await NestFactory.create(AppModule, {
+    logger: new ConsoleLogger({
+      json: true,
+      colors: process.env.NODE_ENV === 'production' ? false : true,
+    }),
+  });
+  await app.listen(process.env.PORT ?? 3333);
 }
 bootstrap();
