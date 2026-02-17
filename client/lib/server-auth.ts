3dc669c5a0255b8ed707dde65618db382dbae9cd
Nigel Nindo <nigelnindodev@gmail.com>
2026-02-17 18:17:05 +0300

    Minor naming changes

--- client/lib/server-auth.ts
+++ client/lib/server-auth.ts
@@ -3,7 +3,7 @@ import { cookies } from 'next/headers';
 export async function hasAuthToken(): Promise<boolean> {
   console.log('Checking has authToken');
   const cookieStore = await cookies();
-  const authToken = cookieStore.get('_nexus_xborg');
+  const authToken = cookieStore.get('_nest_next_scaffold'); // should be a config?
   const result = !!authToken?.value;
   return result;
 }
